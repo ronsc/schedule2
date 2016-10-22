7c8d6cf5040b3651c10240722f985e6e6255e924
ronsc <fdroncpe17@gmail.com>
2016-10-22 20:40:17 +0700

    Initialize Project

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Schedule2Page } from './app.po';
+
+describe('schedule2 App', function() {
+  let page: Schedule2Page;
+
+  beforeEach(() => {
+    page = new Schedule2Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
