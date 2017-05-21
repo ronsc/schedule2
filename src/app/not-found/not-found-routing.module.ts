15c320b8d1cf2470be76f748ea42045ba9339999
ronsc <fdroncpe17@gmail.com>
2017-05-21 10:24:58 +0700

    added not-found module

---
+++ src/app/not-found/not-found-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found.component';
+
+const routes: Routes = [
+  { path: '404', component: NotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class NotFoundRoutingModule { }
