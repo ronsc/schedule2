91b436310645163d42d07b8e1ed553500968b9a8
ronsc <fdroncpe17@gmail.com>
2017-05-21 10:25:35 +0700

    added home module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  // tslint:disable-next-line:component-selector
+  selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  title = 'Schedule2';
 
   constructor() { }
 
