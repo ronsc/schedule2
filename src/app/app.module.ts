5e46284f3b801ee273fdf928bb2b4f0b94e4a513
ronsc <fdroncpe17@gmail.com>
2017-05-21 10:43:22 +0700

    remove angularfire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/layout';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
 import { NotFoundModule } from './not-found/not-found.module';
-
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCv9lUkasSL6ehYyxNpExS07Xl1lNO6GXU',
-  authDomain: 'schedule2-fcf72.firebaseapp.com',
-  databaseURL: 'https://schedule2-fcf72.firebaseio.com',
-  storageBucket: 'schedule2-fcf72.appspot.com',
-  messagingSenderId: '657536714769'
-};
 
 @NgModule({
   declarations: [
@@ -26,7 +17,6 @@ export const firebaseConfig = {
     HomeModule,
     NotFoundModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
