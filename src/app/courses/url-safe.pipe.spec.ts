fadf4fc4f88c8cb1ea1c772b3fb1329cc898917c
ronsc <fdroncpe17@gmail.com>
2017-06-18 21:27:45 +0700

    Remove pipe spec

--- src/app/courses/url-safe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UrlSafePipe } from './url-safe.pipe';
-
-describe('UrlSafePipe', () => {
-  it('create an instance', () => {
-    const pipe = new UrlSafePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
