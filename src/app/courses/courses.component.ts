ef3846b9ef719dc143dc99215cc41c809e6c8cd2
ronsc <fdroncpe17@gmail.com>
2017-07-16 04:07:02 +0700

    hidden html scrollbar

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -4,7 +4,11 @@ import { Component, OnInit } from '@angular/core';
   // tslint:disable-next-line:component-selector
   selector: 'courses',
   templateUrl: './courses.component.html',
-  styles: []
+  styles: [`
+    .columns:last-child {
+      margin-bottom: initial;
+    }
+  `]
 })
 export class CoursesComponent implements OnInit {
 
