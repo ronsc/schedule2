867aafa18ae3142820fe823632ba160ae025263f
Ronnakorn Jannalao <ronnakorn.e@gmail.com>
2020-10-11 20:57:18 +0700

    initial commit

---
+++ apps/schedule-plan2/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ronscme-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'schedule-plan2';
+}
